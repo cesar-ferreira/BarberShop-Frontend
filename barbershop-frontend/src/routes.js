574595e5fa6722a26a78325bab432125fb026ac3
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2018-10-26 21:28:54 -0300

    add base core ui

---
+++ barbershop-frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Loadable from 'react-loadable'
+
+import DefaultLayout from './containers/DefaultLayout';
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+
+// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const routes = [
+  { path: '/', exact: true, name: 'Home', component: DefaultLayout }
+];
+
+export default routes;
